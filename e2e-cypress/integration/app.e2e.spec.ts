3d928ebd0cfec6280c19a9b9fa20d123b7f74e45
Karolos Talvis <sckarolos@users.noreply.github.com>
2021-08-02 19:35:28 +0300

    update e2e

--- e2e-cypress/integration/app.e2e.spec.ts
+++ e2e-cypress/integration/app.e2e.spec.ts
@@ -5,49 +5,27 @@ context('End to end', () => {
             cy.visit('/');
         });
 
-        it("Should display correct heading 'Regions, countries...'", () => {
-            cy.get('app-root .content h1').should('contain.text', 'Regions, countries and country details');
-        });
-
         it('Should display regions dropdown', () => {s
             cy.get('[data-cy=region]').should('be.visible');
             cy.findByRole('option', { name: 'Europe' }).should('be.visible');
             cy.findByRole('option', { name: 'Asia' }).should('be.visible');
         });
 
-        describe("European countries", () => {
-            it("Is possible to show details for 'Finland'", () => {
+        describe("Countries in Europe", () => {
+            it("Is possible to show details for 'Germany'", () => {
                 cy.get('[data-cy=region]').select('Europe');
                 cy.get('[data-cy=country]').find('option')
                     .should('be.visible')
                     .should('have.length.to.be.greaterThan', 50);
-                cy.get('[data-cy=country]').select('Finland');
+                cy.get('[data-cy=country]').select('Germany');
                 cy.get('app-country-details').should('be.visible');
-                cy.get('[data-cy=countryCapital]').should('have.text', 'Helsinki');
+                cy.get('[data-cy=countryCapital]').should('have.text', 'Berlin');
             });
 
-            it("Is possible to show details for 'Spain'", () => {
-                cy.get('[data-cy=country]').select('Spain');
+            it("Is possible to show details for 'Greece'", () => {
+                cy.get('[data-cy=country]').select('Greece');
                 cy.get('app-country-details').should('be.visible');
-                cy.get('[data-cy=countryCapital]').should('have.text', 'Madrid');
-            });
-        });
-
-        describe("Asian countries", () => {
-            it("Is possible to show details for 'India'", () => {
-                cy.get('[data-cy=region]').select('Asia');
-                cy.get('[data-cy=country]').find('option')
-                    .should('be.visible')
-                    .should('have.length.to.be.greaterThan', 50);
-                cy.get('[data-cy=country]').select('India');
-                cy.get('app-country-details').should('be.visible');
-                cy.get('[data-cy=countryCapital]').should('have.text', 'New Delhi');
-            });
-
-            it("Is possible to show details for 'Hong Kong'", () => {
-                cy.get('[data-cy=country]').select('Hong Kong');
-                cy.get('app-country-details').should('be.visible');
-                cy.get('[data-cy=countryCapital]').should('have.text', 'City of Victoria');
+                cy.get('[data-cy=countryCapital]').should('have.text', 'Athens');
             });
         });
     });
